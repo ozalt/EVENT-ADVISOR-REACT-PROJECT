c0e95d25031e3dd716a148ad72d853fcb21b75d9
Sohaib <106882799+Sohaib5712@users.noreply.github.com>
2023-05-21 17:26:51 +0500

    How it work section

--- src/components/HIW_text/HiwText.jsx
+++ src/components/HIW_text/HiwText.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './hiwtext.css';
+import icon from '../../assets/Work.png'
 
-const HiwText = () => {
+const HiwText = ({HiwHeading, HiwText}) => {
     return (
-        <div>
-            How It Word
+        <div className='how-work'>
+            <img src={icon} alt="" />
+            <div className="how-heading">
+                <h1>{HiwHeading}</h1>
+                <p>{HiwText}</p>
+            </div>
         </div>
     )
 }
--- src/containers/HIW/Hiw.jsx
+++ src/containers/HIW/Hiw.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './hiw.css';
+import { HiwText } from '../../components';
+import HiwImg from '../../assets/how_it _work.png'
 const Hiw = () => {
     return (
-        <div>
-            HOw it Work
+        <div className='how-it-work'>
+            <div className="how-it-work-heading">
+                How It Work?
+                <div className="work-design"></div>
+            </div>
+            <div className="work-body">
+                <div className="work-content">
+                    <HiwText HiwHeading={'Create an Account'} HiwText={'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit'} />
+
+                    <HiwText HiwHeading={'More Halp'} HiwText={'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit'} />
+
+                    <HiwText HiwHeading={'Once More'} HiwText={'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit'} />
+                </div>
+                <img src={HiwImg} alt="" className='work-img' />
+            </div>
         </div>
     )
 }
